874381a3e27a7cfd8cea23af6426dc1df9dbca5a
Josh Starcher <josh.starcher@gmail.com>
2012-07-23 11:33:05 -0400

    ability to create and update tasks via api

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,35 @@
 class Api::V1::TasksController < Api::V1::BaseController
 
   def index
-    render json: current_account_list.tasks.includes(:contacts, :activity_comments), callback: params[:callback]
+    render json: tasks, callback: params[:callback]
+  end
+
+  def show
+    render json: tasks.find(params[:id]), callback: params[:callback]
+  end
+
+  def update
+    task = tasks.find(params[:id])
+    if task.update_attributes(params[:task])
+      render json: task, callback: params[:callback]
+    else
+      render json: {errors: task.errors.full_messages}, callback: params[:callback], status: :bad_request
+    end
+  end
+
+  def create
+    task = tasks.new(params[:task])
+    if task.save
+      render json: task, callback: params[:callback], status: :status
+    else
+      render json: {errors: task.errors.full_messages}, callback: params[:callback], status: :bad_request
+    end
+  end
+
+  protected
+
+  def tasks
+    current_account_list.tasks.includes(:contacts, :activity_comments)
   end
 
 end
