7666a29ef26105356753ff59eb59e0c4b8dcc164
Josh Starcher <josh.starcher@gmail.com>
2012-07-24 14:26:24 -0400

    change 'status' to 'phase' for api
    'status' is a reserved word in js

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
   def show
-    user = params[:id] == 'me' ? current_user : User.where(id: current_user.account_lists.includes(:account_list_users).collect(&:account_list_users).flatten.collect(&:user_id)).find(params[:id])
+    user = if params[:id] == 'me'
+             current_user
+           else
+             # Allow a user to see user information for anyone else they share an account list with
+             User.where(id: current_user.account_lists.includes(:account_list_users).collect(&:account_list_users).flatten.collect(&:user_id)).find(params[:id])
+           end
     render json: user, callback: params[:callback]
   end
 
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,12 @@
 class ContactSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :name, :pledge_amount, :pledge_frequency, :pledge_start_date, :status, :deceased,
+  attributes :id, :name, :pledge_amount, :pledge_frequency, :pledge_start_date, :deceased,
              :notes, :notes_saved_at, :next_ask, :never_ask, :likely_to_give, :church_name, :send_newsletter,
              :magazine, :last_activity, :last_appointment, :last_letter, :last_phone_call, :last_pre_call,
              :last_thank
+
+  attribute :status, key: :phase
 
   has_many :people, :addresses
 
