cd1906e6eee89d81e2fb0662aaf318936274acfc
Josh Starcher <josh.starcher@gmail.com>
2012-07-24 10:13:11 -0400

    allow a query for other users (in api)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
   def show
-    render json: current_user, callback: params[:callback]
+    user = params[:id] == 'me' ? current_user : User.where(id: current_user.account_lists.includes(:account_list_users).collect(&:account_list_users).flatten.collect(&:user_id)).find(params[:id])
+    render json: user, callback: params[:callback]
   end
 
 end
