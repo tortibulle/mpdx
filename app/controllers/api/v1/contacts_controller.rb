3a5273c73e1219c10e4e71b2b09be985f51563b0
Josh Starcher <josh.starcher@gmail.com>
2012-07-26 13:46:18 -0400

    fix typo

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ContactsController < Api::V1::BaseController
 
   def index
     order = params[:order] || 'name'
-    render json: contacts.contacts.order(order)
+    render json: contacts.order(order)
                                   .includes({:people => [:email_addresses, :phone_numbers]}, :addresses),
            callback: params[:callback]
   end
