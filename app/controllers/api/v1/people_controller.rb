d994ed62350ce754d4119da3ebcf505221814724
Josh Starcher <josh.starcher@gmail.com>
2012-08-02 14:44:44 -0400

    enable ensure_login on api calls

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::PeopleController < Api::V1::BaseController
 
   def index
-    render json: current_user.people.includes(:phone_numbers, :email_addresses), callback: params[:callback]
+    render json: people, callback: params[:callback]
+  end
+
+  def show
+    render json: people.find(params[:id]), callback: params[:callback]
+  end
+
+  protected
+
+  def people
+    current_account_list.people.includes(:phone_numbers, :email_addresses)
   end
 
 end
