c8fd73bfc8c3c46cd9a753c0279e6a26320309ff
Josh Starcher <josh.starcher@gmail.com>
2012-09-06 11:54:40 -0400

    add avatar to json for contact and person

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,11 +1,17 @@
 class ContactSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::AssetTagHelper
+
   embed :ids, include: true
 
   attributes :id, :name, :pledge_amount, :pledge_frequency, :pledge_start_date, :status, :deceased,
              :notes, :notes_saved_at, :next_ask, :never_ask, :likely_to_give, :church_name, :send_newsletter,
              :magazine, :last_activity, :last_appointment, :last_letter, :last_phone_call, :last_pre_call,
-             :last_thank
+             :last_thank, :avatar
 
   has_many :people, :addresses
+
+  def avatar
+    '/assets/avatar.png'
+  end
 
 end
--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -2,7 +2,12 @@ class PersonSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :first_name, :last_name, :middle_name, :birthday_month, :birthday_year,
              :anniversary_month, :anniversary_year, :anniversary_day, :title, :suffix, :gender,
-             :marital_status, :master_person_id, :birthday_day
+             :marital_status, :master_person_id, :birthday_day, :avatar
 
   has_many :phone_numbers, :email_addresses
+
+  def avatar
+    person_exhibit = Exhibit.exhibit(object, nil)
+    '/assets/' + person_exhibit.avatar
+  end
 end
