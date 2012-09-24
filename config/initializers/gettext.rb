7c655a0823cdfa48197c7ed5d727a1353dd27d60
Josh Starcher <josh.starcher@gmail.com>
2012-09-24 14:26:18 +0300

    add a french file

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -1,4 +1,4 @@
 FastGettext.add_text_domain 'mpdx', :path => 'locale', :type => :po
 FastGettext.default_text_domain = 'mpdx'
-FastGettext.default_available_locales = ['en','de']
+FastGettext.default_available_locales = ['en','de','fr']
 GettextI18nRails.translations_are_html_safe = true
