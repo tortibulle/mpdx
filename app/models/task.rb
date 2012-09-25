8ca5fbdd184b7ee694408471395a9547f33497a8
Josh Starcher <josh.starcher@gmail.com>
2012-09-25 21:34:10 +0300

    try to update gettext stuff to get it to work in js files

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,11 @@ class Task < Activity
   attr_accessible :activity_type
 
   assignable_values_for :activity_type, :allow_blank => true do
-    ['Appointment', 'Call', 'Email', 'Text Message', 'Facebook Message', 'Letter', 'Newsletter',
+    ['Call', 'Appointment', 'Email', 'Text Message', 'Facebook Message', 'Letter', 'Newsletter',
      'Pre Call Letter', 'Reminder Letter', 'Support Letter', 'Thanks', 'To Do']
+  end
+
+  assignable_values_for :result, :allow_blank => true do
+    ['Attempted', 'Done']
   end
 end
