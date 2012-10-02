4309e0842a2ce774a2aa96b2316160199b5f9308
Josh Starcher <josh.starcher@gmail.com>
2012-10-02 17:14:22 -0400

    add ability to remove tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 class TagsController < ApplicationController
   def create
-    if params[:acts_as_taggable_on_tag][:name].present?
-      contacts = current_account_list.contacts.find_all_by_id(params[:contact_ids].split(','))
+    if params[:add_tag_name].present?
+      contacts = current_account_list.contacts.find_all_by_id(params[:add_tag_contact_ids].split(','))
       contacts.each do |c|
-        c.tag_list << params[:acts_as_taggable_on_tag][:name]
+        c.tag_list << params[:add_tag_name]
         c.save
       end
     end
@@ -11,5 +11,13 @@ class TagsController < ApplicationController
   end
 
   def destroy
+    if params[:remove_tag_name].present?
+      contacts = current_account_list.contacts.find_all_by_id(params[:remove_tag_contact_ids].split(','))
+      contacts.each do |c|
+        c.tag_list.delete(params[:remove_tag_name])
+        c.save
+      end
+    end
+    redirect_to :back
   end
 end
