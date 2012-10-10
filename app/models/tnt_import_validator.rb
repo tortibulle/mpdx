e4cc41b3bf4f866b2adf91c090d7a6876e806c9d
Josh Starcher <josh.starcher@gmail.com>
2012-10-10 10:33:57 -0400

    more verbose error message on tnt upload

--- app/models/tnt_import_validator.rb
+++ app/models/tnt_import_validator.rb
@@ -1,4 +1,5 @@
 class TntImportValidator < ActiveModel::Validator
+  include ActionView::Helpers::UrlHelper
   def validate(import)
     if import.file.file
       tnt_import = TntImport.new(import)
@@ -7,7 +8,9 @@ class TntImportValidator < ActiveModel::Validator
 
       # Make sure required columns are present
       unless (lines.headers & TntImport.required_columns).length == TntImport.required_columns.length
-        import.errors[:base] << _('You need to export all the available fields from TNT')
+        import.errors[:base] << _('You need to export all the available fields from TNT. Also make sure you export as a .csv, not the Excel option. If you continue to have issues, make sure you have watched %{link}, then send us an email to %{email} with a copy of your .csv file') %
+          { link: link_to('http://screencast.com/t/lFiYn0EA4', 'http://screencast.com/t/lFiYn0EA4', target: '_blank'),
+            email: mail_to('support@mpdx.org','support@mpdx.org') }
       end
     else
       import.errors[:base] << _('Please choose a file that ends with .csv')
