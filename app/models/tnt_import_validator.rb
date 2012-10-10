1373a32801a73ed7a59f2b23facc24f8da18a450
Josh Starcher <josh.starcher@gmail.com>
2012-10-10 12:21:51 -0400

    change link to tnt screen cast

--- app/models/tnt_import_validator.rb
+++ app/models/tnt_import_validator.rb
@@ -9,7 +9,7 @@ class TntImportValidator < ActiveModel::Validator
       # Make sure required columns are present
       unless (lines.headers & TntImport.required_columns).length == TntImport.required_columns.length
         import.errors[:base] << _('You need to export all the available fields from TNT. Also make sure you export as a .csv, not the Excel option. If you continue to have issues, make sure you have watched %{link}, then send us an email to %{email} with a copy of your .csv file') %
-          { link: link_to('http://screencast.com/t/lFiYn0EA4', 'http://screencast.com/t/lFiYn0EA4', target: '_blank'),
+          { link: link_to('http://youtu.be/hyixke_5Qew', 'http://youtu.be/hyixke_5Qew', target: '_blank'),
             email: mail_to('support@mpdx.org','support@mpdx.org') }
       end
     else
