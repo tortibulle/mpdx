007e93ac5c26f038fc10910d975ac242630ca7c4
Josh Starcher <josh.starcher@gmail.com>
2012-10-11 10:13:22 -0400

    expire a session after one day to force people to
    log back in and refresh their data

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 require 'action_dispatch/middleware/session/dalli_store'
-Mpdx::Application.config.session_store :dalli_store, :memcache_server => ['127.0.0.1'], :namespace => 'sessions', :key => '_mpdx_session', :expire_after => 6.days
+Mpdx::Application.config.session_store :dalli_store, :memcache_server => ['127.0.0.1'], :namespace => 'sessions', :key => '_mpdx_session', :expire_after => 1.day
 
 #Mpdx::Application.config.session_store :cookie_store, :key => '_mpdx_session'
 
