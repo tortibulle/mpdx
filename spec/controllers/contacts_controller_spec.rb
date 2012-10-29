b36e0ff4f956656e89abc75c879ae61ab37afe6e
Josh Starcher <josh.starcher@gmail.com>
2012-10-29 11:08:51 -0400

    remove some unused files

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -8,31 +8,31 @@ describe ContactsController do
       @contact = create(:contact, account_list: @user.account_lists.first)
     end
 
-    describe 'GET index' do
+    describe '#index' do
       before do
         donor_account = create(:donor_account, master_company: create(:master_company))
         @contact2 = create(:contact, name: 'Z', account_list: @user.account_lists.first)
         @contact2.donor_accounts << donor_account
       end
-      it "should get all" do
+      it "gets all" do
         get :index
         response.should be_success
         assigns(:contacts).should == [@contact, @contact2]
       end
 
-      it "should get people" do
+      it "gets people" do
         get :index, filter: 'people'
         response.should be_success
         assigns(:contacts).should == [@contact]
       end
 
-      it "should get companies" do
+      it "gets companies" do
         get :index, filter: 'companies'
         response.should be_success
         assigns(:contacts).should == [@contact2]
       end
 
-      it "should filter by tag" do
+      it "filters by tag" do
         @contact.update_attributes(tag_list: 'asdf')
         get :index, tags: 'asdf'
         response.should be_success
@@ -41,7 +41,7 @@ describe ContactsController do
 
     end
 
-    describe 'GET show' do
+    describe '#show' do
       it "should find a contact in the current account list" do
         get :show, id: @contact.id
         response.should be_success
@@ -49,7 +49,7 @@ describe ContactsController do
       end
     end
 
-    describe 'GET edit' do
+    describe '#edit' do
       it "should edit a contact in the current account list" do
         get :edit, id: @contact.id
         response.should be_success
@@ -57,7 +57,7 @@ describe ContactsController do
       end
     end
 
-    describe 'GET new' do
+    describe '#new' do
       it "should render the new template" do
         get :new
         response.should be_success
@@ -65,7 +65,7 @@ describe ContactsController do
       end
     end
 
-    describe "POST 'create'" do
+    describe "#create" do
       it "should create a good record" do
         -> {
           post :create, contact: {name: 'foo'}
@@ -75,7 +75,7 @@ describe ContactsController do
         }.should change(Contact, :count).by(1)
       end
 
-      it "should not create a contact without a name" do
+      it "doesn't create a contact without a name" do
         post :create, contact: {name: ''}
         assigns(:contact).errors.full_messages.should == ["Name can't be blank"]
         response.should be_success
@@ -83,22 +83,22 @@ describe ContactsController do
 
     end
 
-    describe "PUT 'update'" do
-      it "should update a contact valid" do
+    describe "#update" do
+      it "updates a contact when passed valid attributes" do
         put :update, id: @contact.id, contact: {name: 'Bob'}
         contact = assigns(:contact)
         contact.name.should == 'Bob'
         response.should redirect_to(contact)
       end
 
-      it "should not update an invalid contact" do
+      it "doesn't update a contact when passed invalid attributes" do
         put :update, id: @contact.id, contact: {name: ''}
         assigns(:contact).errors.full_messages.should == ["Name can't be blank"]
         response.should be_success
       end
     end
 
-    describe "GET 'destroy'" do
+    describe "#destroy" do
       it "should destroy a contact" do
         -> {
           delete :destroy, id: @contact.id
