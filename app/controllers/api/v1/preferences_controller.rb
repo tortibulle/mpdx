b12b962cdaf41240bd48d1f2f6491961bec8a704
Josh Starcher <josh.starcher@gmail.com>
2012-12-30 12:07:42 -0500

    Add error handling to preferences API
    If a user has not set up their account on the web version, they will now
    get a message instructing them to do so.

--- app/controllers/api/v1/preferences_controller.rb
+++ app/controllers/api/v1/preferences_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::PreferencesController < Api::V1::BaseController
 
   def index
     preferences = current_user.preferences.except(:setup)
-    preferences[:account_list_id] ||= current_account_list.id
-    preferences[:locale] ||= locale
-    render json: {preferences: preferences}, callback: params[:callback]
+    if current_account_list
+      preferences[:account_list_id] ||= current_account_list.id
+      preferences[:locale] ||= locale
+      render json: {preferences: preferences}, callback: params[:callback]
+    else
+      render json: {errors: _('You need to go to http://mpdx.org and set up your account before using the mobile app.')}, callback: params[:callback]
+    end
   end
 
 end
