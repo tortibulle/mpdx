b2b120ac535e24bbbf116ac13a873fd4afc91482
Josh Starcher <josh.starcher@gmail.com>
2013-01-08 08:56:24 -0500

    Improve error handling on social stream tab.

--- app/controllers/social_streams_controller.rb
+++ app/controllers/social_streams_controller.rb
@@ -4,15 +4,16 @@ class SocialStreamsController < ApplicationController
       @contact = current_account_list.contacts.where(id: params[:contact_id]).first
       @items = []
 
-      if current_user.facebook_account.try(:token) &&
-         !current_user.facebook_account.token_missing_or_expired?
-        @graph = Koala::Facebook::API.new(current_user.facebook_account.token)
+      begin
+        @graph = Koala::Facebook::API.new(current_user.facebook_account.try(:token))
         @contact.people.each do |person|
           person.facebook_accounts.each do |account|
             @items += @graph.get_connections(account.remote_id, "posts").collect { |json| SocialItem.new(json) }
           end
         end
         @items.sort!
+      rescue Koala::Facebook::AuthenticationError
+        @bad_facebook_token = true
       end
     end
   end
