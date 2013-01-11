73a98669529a2d1266b4598eab2ad25e7498b8e5
Josh Starcher <josh.starcher@gmail.com>
2013-01-10 22:20:46 -0500

    Add Passenger block to sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -14,3 +14,10 @@ Sidekiq.configure_server do |config|
   config.failures_default_mode = :exhausted
 end
 
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    Sidekiq.configure_client do |config|
+      config.redis = { :size => 1 }
+    end if forked
+  end
+end
