05d757e0d2b885e61cdeb49d0c342d2ecc3c6bbf
Josh Starcher <josh.starcher@gmail.com>
2013-03-18 17:46:43 -0700

    Allow the user to properly set completed_at when logging a task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < Activity
 
-  before_save :update_completed_at
+  before_validation :update_completed_at
 
   assignable_values_for :activity_type, :allow_blank => true do
     [_('Call'), _('Appointment'), _('Email'), _('Text Message'), _('Facebook Message'),
@@ -20,6 +20,7 @@ class Task < Activity
     def update_completed_at
       if changed.include?('completed')
         self.completed_at ||= completed? ? Time.now : nil
+        self.start_at ||= completed_at
         self.result = 'Done' if result.blank?
       end
     end
