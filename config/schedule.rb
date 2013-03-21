348c4c658a6971936b50249641558981eab93c2e
Josh Starcher <josh.starcher@gmail.com>
2013-03-21 10:28:51 +0700

    Go back to updating all the accounts every two days.
    Something is wrong with my 48th update every 30 minutes logic and I
    don't have time to debug it right now.

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,7 +23,10 @@ job_type :rake,    "cd :path && RAILS_ENV=:environment /usr/local/bin/bundle exe
 job_type :rails,    "cd :path && RAILS_ENV=:environment /usr/local/bin/bundle exec /usr/local/bin/rails :task --silent :output"
 job_type :runner,    "cd :path && RAILS_ENV=:environment /usr/local/bin/bundle exec /usr/local/bin/rails runner :task --silent :output"
 
-every 30.minutes do
+#every 30.minutes do
+  #runner "AccountList.update_linked_org_accounts"
+#end
+every 2.days do
   runner "AccountList.update_linked_org_accounts"
 end
 
