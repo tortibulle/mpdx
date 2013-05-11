517db2a305cac43c82738ea67773b5b5e6f039b5
Josh Starcher <josh.starcher@gmail.com>
2013-05-11 10:07:12 -0400

    Notes might be nil

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -189,7 +189,7 @@ class Contact < ActiveRecord::Base
          end
        end
 
-       self.notes += "\n#{other.notes}"
+       self.notes = "#{notes}\n#{other.notes}"
        self.tag_list += other.tag_list
 
        save(validate: false)
