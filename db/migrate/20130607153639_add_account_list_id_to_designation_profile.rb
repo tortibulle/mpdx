05656c71a87061b851c5d24fb19519ad5348397e
Josh Starcher <josh.starcher@gmail.com>
2013-06-07 14:40:35 -0600

    add nil check

--- db/migrate/20130607153639_add_account_list_id_to_designation_profile.rb
+++ db/migrate/20130607153639_add_account_list_id_to_designation_profile.rb
@@ -7,7 +7,9 @@ class AddAccountListIdToDesignationProfile < ActiveRecord::Migration
     add_column :designation_profiles, :account_list_id, :integer
     add_index :designation_profiles, :account_list_id
     AccountList.find_each do |al|
-      al.designation_profile.update_column(:account_list_id, al.id)
+      if al.designation_profile
+        al.designation_profile.update_column(:account_list_id, al.id)
+      end
     end
   end
 end
