695ebb7341591a0377e88e867ba56dd956aa3d64
Josh Starcher <josh.starcher@gmail.com>
2013-06-10 12:46:54 -0600

    Fix spec with user cached
    Used create instead of build for the factory method

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe UserSerializer do
-  let(:user) { build(:user_with_account) }
+  let(:user) { create(:user_with_account) }
   subject { UserSerializer.new(user).as_json }
 
   it { should include :account_lists }
