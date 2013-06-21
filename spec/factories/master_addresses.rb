7a1e61016657c65eba581299f28ae5480b8b3f75
Josh Starcher <josh.starcher@gmail.com>
2013-06-21 11:14:51 -0600

    Add master address

---
+++ spec/factories/master_addresses.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :master_address do
+    street "MyText"
+    city "MyString"
+    state "MyString"
+    country "MyString"
+    postal_code "MyString"
+  end
+end
