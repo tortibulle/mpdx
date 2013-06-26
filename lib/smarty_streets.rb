7b4752e9194af3c9294065a2dfdbdf580fd1ba81
Josh Starcher <josh.starcher@gmail.com>
2013-06-26 09:29:47 -0600

    strip out characters that smarty streets doesn't like

--- lib/smarty_streets.rb
+++ lib/smarty_streets.rb
@@ -25,13 +25,14 @@ module SmartyStreets
     street2 = parts[1]
 
     params << "street=#{CGI::escape(street1)}" if street1.present?
-    params << "street2=#{CGI::escape(street2)}" if street2.present?
+    params << "street2=#{CGI::escape(street2.gsub(/[><']/, ''))}" if street2.present?
     params << "city=#{CGI::escape(address_hash[:city])}" if address_hash[:city].present?
     params << "state=#{CGI::escape(address_hash[:state])}" if address_hash[:state].present?
     params << "zipcode=#{CGI::escape(zipcode)}" if zipcode.present?
     params << "candidates=2&auth-id=#{auth_id}&auth-token=#{auth_token}"
 
     url += params.join('&')
+    puts url
 
     response = RestClient.get(url, content_type: :json, accept: :json)
 
