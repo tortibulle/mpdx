b4870b8a8baed976d99877a2a3ba4131e8304972
Josh Starcher <josh.starcher@gmail.com>
2013-07-08 14:10:43 -0600

    Don't update contacts as part of migration

--- db/migrate/20130708194848_add_uncompleted_tasks_count_to_contact.rb
+++ db/migrate/20130708194848_add_uncompleted_tasks_count_to_contact.rb
@@ -1,8 +1,8 @@
 class AddUncompletedTasksCountToContact < ActiveRecord::Migration
   def change
     add_column :contacts, :uncompleted_tasks_count, :integer, default: 0, null: false
-    Contact.find_each do |c|
-      c.update_column(:uncompleted_tasks_count, c.tasks.uncompleted.count)
-    end
+    #Contact.find_each do |c|
+      #c.update_column(:uncompleted_tasks_count, c.tasks.uncompleted.count)
+    #end
   end
 end
