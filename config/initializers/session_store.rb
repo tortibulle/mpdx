52c4bed4b53b9c3aeb879855cba866b6804c20bc
Josh Starcher <josh.starcher@gmail.com>
2013-10-07 14:21:10 -0400

    Increase session timeout length

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 require 'action_dispatch/middleware/session/dalli_store'
-Mpdx::Application.config.session_store ActionDispatch::Session::CacheStore, :namespace => 'sessions', :key => '_mpdx_session', :expire_after => 2.hours
+Mpdx::Application.config.session_store ActionDispatch::Session::CacheStore, :namespace => 'sessions', :key => '_mpdx_session', :expire_after => 2.days
 
 #Mpdx::Application.config.session_store :cookie_store, :key => '_mpdx_session'
 
