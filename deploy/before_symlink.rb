c68dff7e8eb76d0a9a834c046748374b74767c73
Josh Starcher <josh.starcher@gmail.com>
2013-10-16 16:35:47 -0400

    Delete redis.yml and symlink the aws version

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -11,6 +11,14 @@ link "#{release_path}/public/assets" do
   to "#{new_resource.deploy_to}/shared/assets"
 end
 
+file "#{release_path}/config/redis.yml" do
+  action :delete
+end
+
+link "#{release_path}/config/redis.yml" do
+  to "#{new_resource.deploy_to}/shared/config/redis.yml"
+end
+
 rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 
