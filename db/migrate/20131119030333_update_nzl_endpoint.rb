51f2fa13d904ae682b9f54bc49dfb47635b3c164
Tataihono Nikora <tataihono.nikora@gmail.com>
2013-11-19 16:37:38 +1300

    Issue #44 Migration Fix Update

--- db/migrate/20131119030333_update_nzl_endpoint.rb
+++ db/migrate/20131119030333_update_nzl_endpoint.rb
@@ -5,12 +5,24 @@ class UpdateNzlEndpoint < ActiveRecord::Migration
 
   	unless cccnz.nil?
 			cccnz.query_ini_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      cccnz.staff_portal_url = "https://tntdataserverasia.com/dataserver/nzl/"
+      cccnz.account_balance_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      cccnz.donations_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      cccnz.addresses_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      cccnz.addresses_by_personids_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      cccnz.profiles_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
   		cccnz.save
   		say "Updated Campus Crusade for Christ - NZ"
   	end
 
   	unless tandem.nil?
-			tandem.query_ini_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      tandem.query_ini_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      tandem.staff_portal_url = "https://tntdataserverasia.com/dataserver/nzl/"
+      tandem.account_balance_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      tandem.donations_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      tandem.addresses_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      tandem.addresses_by_personids_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
+      tandem.profiles_url = "https://tntdataserverasia.com/dataserver/nzl/dataquery/tntquery.aspx"
   		tandem.save
   		say "Updated Tandem Ministries"
   	end
