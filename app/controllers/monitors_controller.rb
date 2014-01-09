ceca38dbf5166acdaf14de3f2c8f056dc7bdad8c
Josh Starcher <josh.starcher@gmail.com>
2014-01-09 10:26:56 -0500

    Don't have newrelic include monitors in calculations

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -2,6 +2,7 @@ class MonitorsController < ApplicationController
   skip_before_filter :ensure_login
   skip_before_filter :ensure_setup_finished
   layout nil
+  newrelic_ignore
 
   def lb
     ActiveRecord::Base.connection.select_values("select id from people limit 1")
