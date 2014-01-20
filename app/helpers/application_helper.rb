85dcbc7b92e615ce5c1b84e8afcc62c3451bc84d
Josh Starcher <josh.starcher@gmail.com>
2014-01-20 15:09:51 -0500

    Fix datetime formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,7 +60,7 @@ module ApplicationHelper
 
   def l(date, options = {})
     options[:format] ||= :short
-    date = date.is_a?(Date) ? date.to_datetime.localize(locale).to_date : date.localize(locale)
+    date = date.is_a?(Date) ? date.to_datetime.localize(locale).to_date : date.to_datetime.localize(locale)
 
     if [:full, :long, :medium, :short].include?(options[:format])
       date.send("to_#{options[:format]}_s".to_sym)
@@ -68,7 +68,7 @@ module ApplicationHelper
       case options[:format]
       when :month_abbrv
         date.to_s(format: 'MMM')
-      when :date_time
+        when :date_time
         date.to_short_s
       else
         date.to_s(format: options[:format])
