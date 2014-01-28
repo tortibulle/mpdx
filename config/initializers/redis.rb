14e425e7299b6f5f549dfe0f57da07e5c1cba6f6
Josh Starcher <josh.starcher@gmail.com>
2014-01-28 14:49:27 -0500

    Fix variable names

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,6 +4,6 @@ require 'redis/namespace'
 rails_root = ENV['RAILS_ROOT'] || Rails.root.to_s
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-resque_config = YAML.load_file(rails_root + '/config/redis.yml')
-host, port = resque_config[rails_env].split(':')
+redis_config = YAML.load_file(rails_root + '/config/redis.yml')
+host, port = redis_config[rails_env].split(':')
 Redis.current = Redis::Namespace.new("MPDX:#{rails_env}", redis: Redis.new(host: host, port: port))
