f73de0cbc50fb27bedd5bee572938700c67ac8e9
Josh Starcher <josh.starcher@gmail.com>
2014-02-27 18:23:15 +0800

    Make sure we only notify people when gifts come to their account
    (not someone else's account)

--- app/models/notification_type/special_gift.rb
+++ app/models/notification_type/special_gift.rb
@@ -3,7 +3,11 @@ class NotificationType::SpecialGift < NotificationType
   def check(account_list)
     notifications = []
     account_list.contacts.where(account_list_id: account_list.id).non_financial_partners.each do |contact|
-      if donation = contact.donations.where("donation_date > ?", 2.weeks.ago).order('donations.donation_date desc').last
+      if donation = contact.donations.
+          where("donation_date > ?", 2.weeks.ago).
+          where(designation_account_id: account_list.designation_accounts.pluck(:id)).
+          order('donations.donation_date desc').
+          last
         prior_notification = Notification.active.where(notification_type_id: id, donation_id: donation.id).first
         unless prior_notification
           notification = contact.notifications.create!(notification_type_id: id, donation_id: donation.id, event_date: Date.today)
