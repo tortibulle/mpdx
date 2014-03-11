0e0a911d2a1f6f4c0921ea007b9cb9d6a3b4aed8
Justin Sabelko <justin.sabelko@cru.org>
2014-03-11 15:45:22 -0500

    Make sure progress report respects time zone

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,8 +6,8 @@ class Task < Activity
 
   scope :of_type, ->(activity_type) { where(activity_type: activity_type) }
   scope :with_result, ->(result) { where(result: result) }
-  scope :completed_between, -> (start_date, end_date) { where("completed_at BETWEEN ? and ?", start_date, end_date) }
-  scope :created_between, -> (start_date, end_date) { where("created_at BETWEEN ? and ?", start_date, end_date) }
+  scope :completed_between, -> (start_date, end_date) { where("completed_at BETWEEN ? and ?", start_date.in_time_zone, (end_date + 1.day).in_time_zone) }
+  scope :created_between, -> (start_date, end_date) { where("created_at BETWEEN ? and ?", start_date.in_time_zone, (end_date + 1.day).in_time_zone) }
 
   PERMITTED_ATTRIBUTES = [
     :starred, :location, :subject, :start_at, :end_at, :activity_type, :result, :completed_at,
