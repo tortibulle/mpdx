f75d9bd7e350d77593618a4f40b01807fb6c3246
Josh Starcher <josh.starcher@gmail.com>
2014-03-20 11:17:07 -0400

    Fix bug updating a task

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -30,4 +30,14 @@ describe Activity do
       expect(starred_tasks).not_to include unstarred_task
     end
   end
+
+  context '#update_attributes' do
+    it 'saves a task with a blank related contact' do
+      task = create(:task)
+      ac = task.activity_contacts.create!
+      expect(
+        task.update_attributes("subject"=>"zvxzcz", "start_at(2i)"=>"12", "start_at(3i)"=>"10", "start_at(1i)"=>"2013", "start_at(4i)"=>"15", "start_at(5i)"=>"15", "activity_type"=>"Call", "tag_list"=>"", "activity_comments_attributes"=>{"0"=>{"body"=>"asdf"}}, "activity_contacts_attributes"=>{"0"=>{"contact_id"=>"", "id"=>ac.id.to_s}})
+      ).to be_true
+    end
+  end
 end
