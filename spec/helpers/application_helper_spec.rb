07dde4b566afc859f917360eed5afa08f1282870
Justin Sabelko <justin.sabelko@cru.org>
2014-04-01 14:14:27 -0500

    Fix Time Zone tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,13 +7,13 @@ describe ApplicationHelper do
     end
     it "should output the right time zone for activity_comment.created_at" do #activity_comments/_comment.html.erb
       comment = create(:activity_comment)
-      comment_str = comment.created_at.strftime('%-m/%d/%y, %-l:%M %p')
+      comment_str = comment.created_at.strftime('%-m/%-d/%y, %-l:%M %p')
       output = helper.l(comment.created_at)
       comment_str.should == output
     end
     it "should output the right time zone for contact.notes_saved_at" do #contacts/_contact_body.html.erb & update.js.erb
       contact = create(:contact)
-      notes_str = contact.notes_saved_at.strftime('%-m/%d/%y, %-l:%M %p')
+      notes_str = contact.notes_saved_at.strftime('%-m/%-d/%y, %-l:%M %p')
       output = helper.l(contact.notes_saved_at.to_datetime)
       notes_str.should == output
 
@@ -25,7 +25,7 @@ describe ApplicationHelper do
       task.completed_at = Time.zone.now
       task.save
 
-      task_str = task.completed_at.strftime('%-m/%d/%y, %-l:%M %p')
+      task_str = task.completed_at.strftime('%-m/%-d/%y, %-l:%M %p')
       output = helper.l(task.completed_at)
       task_str.should == output
     end
