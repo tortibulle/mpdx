6a38daa088f8066654b6105d84c34bf462b3ff60
Justin Sabelko <justin.sabelko@cru.org>
2014-04-09 12:54:34 -0500

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Mpdx::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = { exclude: proc { |env| env['PATH_INFO'].start_with?('/up_with_db') } }
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
