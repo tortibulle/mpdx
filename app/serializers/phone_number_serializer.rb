bdaf4f6976aeba45946ee3dc3b7b86cc33478060
Spencer Oberstadt <soberstadt@gmail.com>
2014-04-15 14:26:30 -0500

    send formatted phone number format on api

--- app/serializers/phone_number_serializer.rb
+++ app/serializers/phone_number_serializer.rb
@@ -1,6 +1,13 @@
 class PhoneNumberSerializer < ActiveModel::Serializer
+  include DisplayCase::ExhibitsHelper
+
   embed :ids, include: true
   ATTRIBUTES = [:id, :number, :country_code, :location, :primary, :created_at, :updated_at]
   attributes *ATTRIBUTES
+
+  def number
+    phone_number_exhibit = exhibit(object)
+    phone_number_exhibit.number
+  end
 
 end
