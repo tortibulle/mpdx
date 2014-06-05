eefe2def6c3b11c2d8c88e885e7fec41f4929b8e
Spencer Oberstadt <soberstadt@gmail.com>
2014-06-05 13:46:38 -0500

    add API::Preferences#update

--- app/controllers/api/v1/preferences_controller.rb
+++ app/controllers/api/v1/preferences_controller.rb
@@ -7,4 +7,15 @@ class Api::V1::PreferencesController < Api::V1::BaseController
     render json: {preferences: preferences}, callback: params[:callback]
   end
 
+  def update
+    account_list = current_user.account_lists.find(params[:id])
+    account_list ||= current_account_list
+    @preference_set = PreferenceSet.new(params[:preference_set].merge!(user: current_user, account_list: account_list))
+    if @preference_set.save
+      render json: {preferences: @preference_set}, callback: params[:callback]
+    else
+      render json: {errors: @preference_set.errors.full_messages}, callback: params[:callback], status: :bad_request
+    end
+  end
+
 end
