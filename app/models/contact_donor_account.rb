61edd702339d8603395f4cada54fd25e67377d35
Justin Sabelko <justin.sabelko@cru.org>
2014-06-09 16:46:56 -0500

    Make sure donor IDs are unique in organizations without automatic sync

--- app/models/contact_donor_account.rb
+++ app/models/contact_donor_account.rb
@@ -6,8 +6,15 @@ class ContactDonorAccount < ActiveRecord::Base
 
   def ensure_one_contact_per_donor_account_number
     if contact.account_list.donor_accounts.where(account_number: donor_account.account_number).first
-      contact.errors.add(:base, _("Another contact on your accout already has the donor account number you've tried to assign to this contact."))
+      contact.errors.add(:base, _("Another contact on your account already has the donor account number you've tried to assign to this contact."))
       false
+    end
+    # Organization either doesn't sync automatically or doesn't support donors with same ID
+    if contact.account_list.designation_accounts.size == 0
+      if donor_account.organization.donor_accounts.where(account_number: donor_account.account_number).first
+        contact.errors.add(:base, _("Another contact in your organization already has the donor account number you've tried to assign to this contact.  Please make it more unique."))
+        false
+      end
     end
     true
   end
