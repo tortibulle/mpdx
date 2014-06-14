3a36161b8a4f37872ca7174a4999941c9356845a
Spencer Oberstadt <soberstadt@gmail.com>
2014-06-13 22:08:18 -0400

    clean up code a little

--- app/models/contact_donor_account.rb
+++ app/models/contact_donor_account.rb
@@ -5,16 +5,14 @@ class ContactDonorAccount < ActiveRecord::Base
   validate :ensure_one_contact_per_donor_account_number
 
   def ensure_one_contact_per_donor_account_number
-    if contact.account_list.donor_accounts.where(account_number: donor_account.account_number).first
+    if contact.account_list.donor_accounts.where(account_number: donor_account.account_number).any?
       contact.errors.add(:base, _("Another contact on your account already has the donor account number you've tried to assign to this contact."))
       false
     end
     # Organization either doesn't sync automatically or doesn't support donors with same ID
-    if contact.account_list.designation_accounts.size == 0
-      if donor_account.organization.donor_accounts.where(account_number: donor_account.account_number).first
-        contact.errors.add(:base, _("Another contact in your organization already has the donor account number you've tried to assign to this contact.  Please make it more unique."))
-        false
-      end
+    if contact.account_list.designation_accounts.empty? && donor_account.organization.donor_accounts.where(account_number: donor_account.account_number).any?
+      contact.errors.add(:base, _("Another contact in your organization already has the donor account number you've tried to assign to this contact.  Please make it more unique."))
+      false
     end
     true
   end
