5fbb946009ce8fa0c35f8b77949221cb9076e3c4
Spencer Oberstadt <soberstadt@gmail.com>
2014-07-16 17:12:49 -0500

    add oberstadt to sidekiq constraint

--- config/routes.rb
+++ config/routes.rb
@@ -144,7 +144,7 @@ Mpdx::Application.routes.draw do
   constraint = lambda { |request| request.env["rack.session"] and
                                   request.env["rack.session"]["warden.user.user.key"] and
                                   request.env["rack.session"]["warden.user.user.key"][0] and
-                                  ['Starcher','Sabelko'].include?(User.find(request.env["rack.session"]["warden.user.user.key"][0].first).last_name) }
+                                  ['Starcher','Sabelko','Oberstadt'].include?(User.find(request.env["rack.session"]["warden.user.user.key"][0].first).last_name) }
   constraints constraint do
     mount Sidekiq::Web => '/sidekiq'
   end
