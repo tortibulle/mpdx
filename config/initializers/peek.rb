44ccef930c0421006cf03f11d1d26d0d3d080a8b
Josh Starcher <josh.starcher@gmail.com>
2014-07-18 19:38:42 -0400

    remove peek-sidekiq

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,7 +1,6 @@
 Peek.into Peek::Views::Git, :nwo => 'CruGlobal/mpdx'
 Peek.into Peek::Views::PG
 Peek.into Peek::Views::Dalli
-Peek.into Peek::Views::Sidekiq
 Peek.into Peek::Views::Redis
 Peek.into Peek::Views::PerformanceBar
 Peek.into Peek::Views::GC
