9cef5dcc6bea27aa6bf56fd957b54e8077907868
adammeyer <adam.meyer@cru.org>
2014-07-22 15:24:59 -0400

    Edit task completed date from contact History tab

--- app/views/tasks/edit.js
+++ app/views/tasks/edit.js
@@ -8,9 +8,23 @@ $('[data-calendar]').datepicker({
         var month = dateText.substr(5,2);
         var day = dateText.substr(8,2);
 
+      if($('[id$=start_at_1i]').length){
         $('[id$=start_at_1i]').val(year);
         $('[id$=start_at_2i]').val(month);
         $('[id$=start_at_3i]').val(day);
+      }
+
+      if($('[id$=completed_at_1i]').length){
+        $('[id$=completed_at_1i]').val(year);
+        $('[id$=completed_at_2i]').val(month);
+        $('[id$=completed_at_3i]').val(day);
+      }
     }
 });
-$('[data-calendar]').datepicker('setDate', $('#modal_task_start_at_1i').val() + '-' + $('#modal_task_start_at_2i').val() + '-' + $('#modal_task_start_at_3i').val());
+if($('#modal_task_start_at_1i').length) {
+  $('[data-calendar]').datepicker('setDate', $('#modal_task_start_at_1i').val() + '-' + $('#modal_task_start_at_2i').val() + '-' + $('#modal_task_start_at_3i').val());
+}
+
+if($('#modal_task_completed_at_1i').length) {
+  $('[data-calendar]').datepicker('setDate', $('#modal_task_completed_at_1i').val() + '-' + $('#modal_task_completed_at_2i').val() + '-' + $('#modal_task_completed_at_3i').val());
+}
