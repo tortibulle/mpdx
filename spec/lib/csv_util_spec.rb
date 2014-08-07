9b168d9bb630cba3e4c6ef8dbcf13abdc932c54b
David Raffensperger <contact@davidraff.com>
2014-08-06 20:35:20 -0400

    Indented HTML tables in CSVUtil spec

--- spec/lib/csv_util_spec.rb
+++ spec/lib/csv_util_spec.rb
@@ -4,55 +4,55 @@ describe CSVUtil do
   describe 'html_table_to_csv' do
     it 'should convert html tables to csv' do
       expect(CSVUtil.html_table_to_csv <<-EOS
-<table>
-  <tr><td>A</td><td>B</td></tr>
-  <tr><td>1</td><td>2</td></tr>
-</table>
-      EOS
-      ).to eq(<<-EOS
+        <table>
+          <tr><td>A</td><td>B</td></tr>
+          <tr><td>1</td><td>2</td></tr>
+        </table>
+              EOS
+              ).to eq(<<-EOS
 A,B
 1,2
-      EOS
+        EOS
       )
 
       expect(CSVUtil.html_table_to_csv <<-EOS
-<table>
-  <tr><td>A</td><td>B</td></tr>
-  <tr><td>1,
-"</td><td>2</td></tr>
-</table>
-      EOS
-      ).to eq(<<-EOS
+        <table>
+          <tr><td>A</td><td>B</td></tr>
+          <tr><td>1,
+        "</td><td>2</td></tr>
+        </table>
+              EOS
+              ).to eq(<<-EOS
 A,B
 "1,
 """,2
-      EOS
+        EOS
       )
 
       expect(CSVUtil.html_table_to_csv <<-EOS
-<table>
-  <thead>
-    <tr>
-        <th>
-A
-</th>
-        <th> B </th>
-    </tr>
-  </thead>
-  <tbody>
-      <tr>
-        <td>
+        <table>
+          <thead>
+            <tr>
+                <th>
+        A
+        </th>
+                <th> B </th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+                <td>
 
-1  </td>
-        <td>2</td>
-      </tr>
-  </tbody>
-</table>
-      EOS
+        1  </td>
+                <td>2</td>
+              </tr>
+          </tbody>
+        </table>
+        EOS
       ).to eq(<<-EOS
 A,B
 1,2
-      EOS
+        EOS
       )
     end
   end
