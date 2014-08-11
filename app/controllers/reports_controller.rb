515a3c31d765fd5673aed2d497b952e88faf3035
David Raffensperger <contact@davidraff.com>
2014-08-11 17:25:21 -0400

    Added average column to contribution report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,6 @@
 require 'csv_util'
 
 class ReportsController < ApplicationController
-  include LocalizationHelper
-
   def contributions
     @page_title = _('Contribution Report')
 
@@ -45,8 +43,8 @@ class ReportsController < ApplicationController
     @raw_donations.each do |donation|
       @donations[donation.donor_account_id] ||= {
         donor: donation.name, id: donation.contact_id, status: donation.status,
-        pledge_amount: number_to_current_currency(donation.pledge_amount),
-        pledge_frequency: Contact.pledge_frequencies[donation.pledge_frequency],
+        pledge_amount: donation.pledge_amount,
+        pledge_frequency: donation.pledge_frequency,
         amounts: {}, total: 0
         }
 
@@ -60,6 +58,31 @@ class ReportsController < ApplicationController
       @sum_row[donation.date_trunc.strftime '%b %y'] ||= 0
 
       @sum_row[donation.date_trunc.strftime '%b %y'] += donation.tendered_amount
+    end
+
+    @donations.each do |key, row|
+      if not row[:pledge_frequency].nil? && row[:pledge_frequency].to_f <= 1.0
+        # If someone gives monthly and they gave, e.g. $50/month for the past four month
+        # and not before that, then assume they are a new ministry partner and their average
+        # should be $50/month.
+        # Thus we find the first month in the report (from left to right) where they
+        # gave a donation and exclude earlier months from the average.
+        month_of_first_donation = (0..11).to_a.reverse.find_index {|index|
+          not row[:amounts][index.month.ago(@end_date).strftime '%b %y'].nil?
+        }
+        months_for_average = 12.0 - month_of_first_donation
+
+        # If there was no donation in the current month, then don't count the current
+        # month toward the average calculation, as it may be midway through the month
+        # and their donation hasn't shown up yet.
+        if row[:amounts][0.month.ago(@end_date).strftime '%b %y'].nil?
+          months_for_average -= 1.0
+        end
+      else
+        months_for_average = 12.0
+      end
+
+      row[:average] = row[:total] / months_for_average
     end
 
     respond_to do |format|
