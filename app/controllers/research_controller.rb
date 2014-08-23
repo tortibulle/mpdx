05e7e43888f604496297dd8d91f1f157c480442e
Josh Starcher <josh.starcher@gmail.com>
2014-08-23 13:49:50 -0400

    Add some more expiramental research functionality.
    This doesn't show up publicly.

--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -4,10 +4,15 @@ class ResearchController < ApplicationController
 
   def search
     @contact = current_account_list.contacts.find(params[:id])
+    account_numbers = @contact.donor_accounts.pluck(:account_number)
     @donor_data = if @contact.donor_accounts.present?
-                    SiebelDonations::Donor.find(ids: @contact.donor_accounts.map(&:account_number).join(','))
+                    SiebelDonations::Donor.find(ids: account_numbers.join(','))
                   else
                     []
                   end
+
+    @other_contacts = Contact.joins(:donor_accounts)
+      .where('donor_accounts.account_number' => account_numbers)
+      .where.not('contacts.id' => @contact.id)
   end
 end
