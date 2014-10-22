a6a4aa1edc7eb7bf9f12d30cb6d902d61e38cc44
adammeyer <adam.meyer@cru.org>
2014-10-22 11:05:49 -0400

    rubocop fixes

--- app/models/appeal_contact.rb
+++ app/models/appeal_contact.rb
@@ -1,7 +1,7 @@
 class AppealContact < ActiveRecord::Base
   has_paper_trail on: [:destroy],
-      meta: { related_object_type: 'Appeal',
-      related_object_id: :appeal_id }
+                  meta: { related_object_type: 'Appeal',
+                          related_object_id: :appeal_id }
 
   belongs_to :appeal, foreign_key: 'appeal_id'
   belongs_to :contact
