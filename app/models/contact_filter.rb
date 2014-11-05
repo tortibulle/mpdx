21e880bec26767699c2d153c79da88284c1e9c0e
draffensperger <draff8660@gmail.com>
2014-11-05 11:35:16 -0500

    Search for person name by Last, First, simplify condition

--- app/models/contact_filter.rb
+++ app/models/contact_filter.rb
@@ -140,10 +140,15 @@ class ContactFilter
       end
 
       if @filters[:wildcard_search].present? && @filters[:wildcard_search] != 'null'
-        first_name, last_name = @filters[:wildcard_search].split
-        if first_name.present? && last_name.present?
-          first_name = first_name.downcase
-          last_name = last_name.downcase
+        if @filters[:wildcard_search].include?(',')
+          last_name, first_name = @filters[:wildcard_search].split(',')
+        else
+          first_name, last_name = @filters[:wildcard_search].split
+        end
+
+        if last_name.present?
+          first_name = first_name.downcase.strip
+          last_name = last_name.downcase.strip
           person_search = ' OR (lower(people.first_name) like :first_name AND lower(people.last_name) like :last_name)'
         else
           person_search = ''
--- spec/models/contact_filter_spec.rb
+++ spec/models/contact_filter_spec.rb
@@ -24,12 +24,12 @@ describe ContactFilter do
       filtered_contacts.should include has_status
     end
 
-    it 'filters by person name on wildcard search' do
+    it 'filters by person name on wildcard search with and without comma' do
       c = create(:contact, name: 'Doe, John')
       p = create(:person, first_name: 'John', last_name: 'Doe')
       c.people << p
-      cf = ContactFilter.new(wildcard_search: 'john doe')
-      expect(cf.filter(Contact)).to include c
+      expect(ContactFilter.new(wildcard_search: 'john doe').filter(Contact)).to include c
+      expect(ContactFilter.new(wildcard_search: ' Doe,  John ').filter(Contact)).to include c
     end
 
     it 'does not cause an error if wildcard search less than two words do' do
