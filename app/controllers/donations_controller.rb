bada8a7a59ece3a6c6d7682397d4f0c434178eab
Justin Sabelko <justin.sabelko@cru.org>
2014-11-11 12:17:23 -0600

    Use before_action

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,11 +2,9 @@ class DonationsController < ApplicationController
   before_action :find_donation, only: [:edit, :destroy, :update]
   before_action :find_contact
   before_action :find_donor_accounts, only: [:edit, :new]
-  before_action :find_appeals, only: [:edit, :new]
+  before_action :find_appeals, only: [:index, :edit, :new]
 
   def index
-    @appeals = current_account_list.appeals
-
     if @contact
       if @contact.donor_account_ids.present?
         @all_donations = current_account_list.donations.where(donor_account_id: @contact.donor_account_ids)
