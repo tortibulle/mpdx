29b23bc5d3bf5d2b8775e90b015cf572eaaca50a
Josh Starcher <josh.starcher@gmail.com>
2014-11-26 13:21:17 -0500

    Add a 20s timeout on prayerletters.com calls

--- app/models/prayer_letters_account.rb
+++ app/models/prayer_letters_account.rb
@@ -165,7 +165,7 @@ class PrayerLettersAccount < ActiveRecord::Base
   def oauth2_request(method, path, params = nil)
     return unless active?
 
-    RestClient::Request.execute(method: method, url: SERVICE_URL + path, payload: params,
+    RestClient::Request.execute(method: method, url: SERVICE_URL + path, payload: params, timeout: 20,
                                 headers: { 'Authorization' => "Bearer #{ URI.encode(oauth2_token) }" })
   rescue RestClient::Unauthorized
     handle_bad_token
