c38ad499318986e6bb2ef08dcd5b243d894b8ece
draffensperger <draff8660@gmail.com>
2014-12-15 08:25:27 -0500

    Doubled prayerletters timeout

--- app/models/prayer_letters_account.rb
+++ app/models/prayer_letters_account.rb
@@ -144,7 +144,7 @@ class PrayerLettersAccount < ActiveRecord::Base
   def get_response(method, path, params = nil)
     return unless active?
 
-    RestClient::Request.execute(method: method, url: SERVICE_URL + path, payload: params, timeout: 480,
+    RestClient::Request.execute(method: method, url: SERVICE_URL + path, payload: params, timeout: 960,
                                 headers: { 'Authorization' => "Bearer #{ URI.encode(oauth2_token) }" })
   rescue RestClient::Unauthorized
     handle_bad_token
